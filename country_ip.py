94c18b00baf4d0938b802f237bb3b2440aee91b4
slarinier <slarinier@bt.foo.org>
2012-09-13 17:09:19 +0200

    correct bug to read file

--- country_ip.py
+++ country_ip.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-#Author: Sebdraven
+
 
 import dschield
 import sys
@@ -7,11 +7,10 @@ import sys
 
 filetoload=sys.argv[1]
 
-fr=open(filetoload,'r')
-ds=dschield.dschield('http://dshield.org/ipinfo_ascii.html?ip=')
-while 1:
 
-	t=fr.readline()
+ds=dschield.dschield('http://dshield.org/ipinfo_ascii.html?ip=')
+with open(filetoload,'r') as fr:
+    for t in fr:
 	ip,country,asname=ds.response(t.strip())
 	print ip +';'+country + ';'+asname
 	
